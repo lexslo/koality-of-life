c07f61ab0c4226ff8246ae85e1cf00cb290d3ba4
Sparkle Party <lexi.slovik@gmail.com>
2021-12-12 14:05:27 -0800

    enable/disable buttons when user types

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
-// api key for IP address
-// https://api.ipstack.com/134.201.250.155?access_key=5eb2ffeb687e846fd6b8eb7245538ec9
+// initialize foundation
+$(document).foundation();
 
 var getUserLocation = function () {
 
@@ -9,9 +9,6 @@ var getUserLocation = function () {
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data){
-                    console.log(data);
-                    console.log(data.city);
-                    console.log(data.region_code);
                     displayUserLocation(data.city, data.region_code);
                 });
             } else {
@@ -21,7 +18,33 @@ var getUserLocation = function () {
 }
 
 var displayUserLocation = function (city, state) {
+    // set text display in card header to user location
     $("#user-ip-location").text(city + ", " + state);
+    // auto fill search input with current location
+    $("#user-entry-location").val(city + ", " + state);
+    // disable search button unless user wishes to change start city
+    $("#search-btn").addClass("disabled");
 }
 
+// enable search button if user goes to type in the first input field
+$("#user-entry-location").on("click", function() {
+    if($(this).is(":focus")) {
+        $("#search-btn").removeClass("disabled");
+    }
+});
+
+// enable compare button when user types in input field
+$("#user-entry-comparison").on("click", function() {
+    if ($(this).is(":focus")) {
+        $("#compare-btn").removeClass("disabled");
+    }
+});
+// disable compare button if empty input field
+$("#user-entry-comparison").on("blur", function() {
+    if ($(this).val() == "") {
+        $("#compare-btn").addClass("disabled");
+    }
+})
+
 //getUserLocation();
+displayUserLocation("Oakland", "CA");
