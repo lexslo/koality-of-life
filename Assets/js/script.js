25fada95445bc58e1e071bb989b6bb61ec0dd686
Charlie H <charltonhua@gmail.com>
2021-12-14 14:10:30 -0800

    add ipstack, teleport setup

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,92 @@
-"hello charlton";
-"hello";
-// LEX
+var ipStackKey = "5eb2ffeb687e846fd6b8eb7245538ec9";
+var ipStack = `http://api.ipstack.com/check?access_key=${ipStackKey}`;
+var teleportCitySearch = `https://api.teleport.org/api/cities/?search=`;
+var userInputSearchForCityEl = document.querySelector("#user-entry-location");
+var userResultsEl = document.querySelector("#user-results");
+
+$(document).ready(function () {
+  fetch(ipStack)
+    .then(function (response) {
+      response.json().then(function (data) {
+        city(data);
+      });
+    })
+    .catch(function (data) {
+      alert("Your location was not found, please use search instead.");
+    });
+});
+
+function city(ipLocation) {
+  var city = ipLocation.city;
+  teleportURL = `${teleportCitySearch}queens`;
+  console.log(teleportURL);
+
+  fetch(teleportURL).then(function (response) {
+    response.json().then(function (data) {
+      obtainUrbanCityScores(data);
+      // console.log(data);
+    });
+  });
+  // .catch(function (data) {
+  //   alert("Your location was not found, please use search instead.");
+  // });
+}
+
+// function manualUserCityEntry() {
+//   $("#search-for-cty").on("submit", function (event) {
+//     event.preventDefault();
+
+//     var cityname = userInputSearchForCityEl.value.trim();
+
+//     if (cityname) {
+//       getCityCoordinates(cityname, true);
+//       console.log(cityname);
+//       cityInputEl.value = "";
+//     } else {
+//       alert("Please enter a City destination");
+//     }
+//   });
+// }
+
+function obtainUrbanCityScores(data) {
+  // console.log(data);
+  var embeddedHREF =
+    data._embedded["city:search-results"][0]["_links"]["city:item"]["href"];
+  // console.log(embeddedHREF);
+
+  // var urbanCity = embeddedHREF["city:urban_area"];
+
+  fetch(embeddedHREF).then(function (response) {
+    response.json().then(function (data) {
+      // console.log(data);
+      urbanCityName = data["_links"]["city:urban_area"]["name"];
+      console.log(urbanCityName);
+      displayUrbanCityData(urbanCityName);
+      // urbanCityScores = urbanCityHref + "scores";
+
+      // fetch(urbanCityScores).then(function (response) {
+      //   response.json().then(function (data) {
+      //     // console.log(data);
+      //   });
+      // });
+    });
+  });
+}
+
+function displayUrbanCityData(urbanCityName) {
+  var cityName = urbanCityName.replaceAll(" ", "-");
+  console.log(cityName);
+  var lifeQualityScores = `<script
+  async
+  class="teleport-widget-script"
+  id="life-quality-score"
+  data-url="https://teleport.org/cities/${cityName}/widget/scores/?currency=USD"
+  data-max-width="770"
+  data-height="950"
+  src="https://teleport.org/assets/firefly/widget-snippet.min.js"
+  ></script>`;
+
+  // // console.log(lifeQualityScores);
+
+  $("#user-results").append(lifeQualityScores);
+}
